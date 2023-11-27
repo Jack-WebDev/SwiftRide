87f7f7c30190d830e941388d5ddb6012c9debf22
Jack <jackwebdev31@gmail.com>
2023-11-27 10:19:03 +0200

    User can edit and delete lifts

--- lib/views/OfferLift/activity.dart
+++ lib/views/OfferLift/activity.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -9,6 +10,20 @@ class Activities extends StatefulWidget {
 }
 
 class _ActivitiesState extends State<Activities> {
+  CollectionReference _listDetails =
+      FirebaseFirestore.instance.collection("offerlift");
+
+  List<String> IDs = [];
+
+  Future getIDs() async {
+    await FirebaseFirestore.instance
+        .collection("offerlift")
+        .get()
+        .then((snapshot) => snapshot.docs.forEach((val) {
+              IDs.add(val.reference.id);
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,61 +49,93 @@ class _ActivitiesState extends State<Activities> {
               child: Padding(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Your Activity",
-                        style: TextStyle(
-                            fontSize: 40, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Column(
-                        children: [
-                          Text(
-                            "Your Lifts",
-                            style: TextStyle(fontSize: 25),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            "Your Past Rides:",
-                            style: TextStyle(fontSize: 25),
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          _card("12 Common Place Str")
-                        ],
-                      ),
-                    ]),
+                child: FutureBuilder(
+                  future: getIDs(),
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                        itemCount: IDs.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: _card(_listDetails.doc(IDs[index]).get()),
+                          );
+                        });
+                  },
+                ),
               ),
             ))));
   }
 }
 
-Widget _card(String streetName) {
-  return Container(
-    decoration: BoxDecoration(
-        border: Border.all(color: Colors.white, width: 2),
-        borderRadius: BorderRadius.circular(15)),
-    child: Padding(
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          Text(streetName),
-          const Text("23:03 PM - 18/12/2023"),
-          const Text("R0.00 - Cancelled")
-        ],
-      ),
-    ),
+Widget _card(Future<DocumentSnapshot> listDetails) {
+  CollectionReference _listDetails =
+      FirebaseFirestore.instance.collection("offerlift");
+
+  Future<void> updateLift() {
+    return _listDetails
+        .doc("3fFOg93r40UpW55uwcSu")
+        .update({
+          "Driver Name": "Jack",
+        })
+        .then((value) => print("User Updated"))
+        .catchError((error) => print("Failed to update user: $error"));
+  }
+
+  Future<void> deleteUser() {
+    return _listDetails
+        .doc('3fFOg93r40UpW55uwcSu')
+        .delete()
+        .then((value) => print("User Deleted"))
+        .catchError((error) => print("Failed to delete user: $error"));
+  }
+
+  return FutureBuilder(
+    future: listDetails,
+    builder: (context, snapshot) {
+      if (snapshot.hasError) {
+        return Text("Something went wrong");
+      }
+
+      if (snapshot.hasData && !snapshot.data!.exists) {
+        return Text("Document does not exist");
+      }
+
+      if (snapshot.connectionState == ConnectionState.done) {
+        Map<String, dynamic> data =
+            snapshot.data!.data() as Map<String, dynamic>;
+        return Column(
+          children: [
+            Text("Driver Name: ${data["Driver Name"]}"),
+            SizedBox(
+              height: 5,
+            ),
+            Text("Car Capacity: ${data["Car Capacity"]}"),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        updateLift();
+                      },
+                      child: Text("Edit Lift")),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        deleteUser();
+                      },
+                      child: Text("Delete Lift"))
+                ],
+              ),
+            )
+          ],
+        );
+      }
+
+      return Text("loading");
+    },
   );
 }
